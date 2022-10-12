9bd7a1e0604498fab96d2d59b92fb48a22cd4827
Mr. Gambit <tyrese968@gmail.com>
2022-10-12 15:19:52 +0100

    First commit

--- JavaIntermediate/src/Serialization/Product.java
+++ JavaIntermediate/src/Serialization/Product.java
@@ -1,5 +1,19 @@
 package Serialization;
 
-public class Product {
+import java.io.Serializable;
+
+public class Product  implements Serializable{
+	public static final String prodId = null;
+	public int prodid;
+	public String name;
+	public transient int price;
+	public String brand;
+	public Product(int prodid, String name, int price, String brand) {
+		super();
+		this.prodid = prodid;
+		this.name = name;
+		this.price = price;
+		this.brand = brand;
+	}
 
 }
--- JavaIntermediate/src/Serialization/test.java
+++ JavaIntermediate/src/Serialization/test.java
@@ -1,9 +1,34 @@
 package Serialization;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Product prod = new Product(1001, "TV", 1000, "Apple");
+		try {
+			
+			FileOutputStream fileOut = new FileOutputStream("object.txt");
+			
+			ObjectOutputStream oOut = new ObjectOutputStream(fileOut);
+			
+			oOut.writeObject(prod);
+			
+			FileInput fileIn = new FileInputStream(fileIn);
+			objectInputStream oin = new ObjectInputStream(fileIn);
+			Product prodCopy = (Product) oin.readObject();
+			System.out.println(Product.prodId);
+			System.out.println(prodCopy.name);
+			System.out.println(prodCopy.brand);
+			System.out.println(prodCopy.price);
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 
 	}
 
